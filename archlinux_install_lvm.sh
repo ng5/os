be83d3bb684a76ae3e2b34bda96b8d81f0da7527
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2021-11-29 16:08:26 +0000

    cleanup

---
+++ archlinux_install_lvm.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+IS_INTEL=$(lscpu | grep -c GenuineIntel)
+if [[ "$IS_INTEL" -ge 1 ]]; then
+    IS_INTEL=true
+else
+    IS_INTEL=false
+fi
+IS_AMD=$(lscpu | grep -c AuthenticAMD)
+if [[ "$IS_AMD" -ge 1 ]]; then
+    IS_AMD=true
+else
+    IS_AMD=false
+fi
+echo "IS_INTEL - $IS_INTEL"
+echo "IS_AMD - $IS_AMD"
+if [[ $IS_INTEL ]]; then
+    echo "intel cpu"
+fi
