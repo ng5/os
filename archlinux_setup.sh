3023803460f015fb7f32f84675b33ae046a23567
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2021-12-06 13:36:52 +0000

    install rust

--- archlinux_setup.sh
+++ archlinux_setup.sh
@@ -122,6 +122,7 @@ __internal_pacman_install ebtables
 __internal_pacman_install dnsmasq
 __internal_pacman_install bridge-utils
 __internal_pacman_install openbsd-netcat
+__internal_pacman_install rust
 
 # enable docker & libvirtd
 sudo usermod -a -G docker "$(whoami)"
