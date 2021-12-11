52c417e7a2328aacc3fe131d8dfad80d03aedfcd
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2021-12-11 16:09:47 +0000

    batch

--- debian_setup.sh
+++ debian_setup.sh
@@ -20,7 +20,7 @@ chown build:build /home/build/.hushlogin
 
 # Install docker
 apt install -y ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
