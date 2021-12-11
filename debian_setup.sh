2c51ba7b0d1ed5a4946619b68be97b141ba5dc65
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2021-12-11 16:56:44 +0000

    formatting

--- debian_setup.sh
+++ debian_setup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 function installZsh() {
-
     curl https://raw.githubusercontent.com/ng5/os/main/.zshenv >"$HOME"/.zshenv
     curl https://raw.githubusercontent.com/ng5/os/main/.zshrc >"$HOME"/.zshrc
     if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
