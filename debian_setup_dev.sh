b832b00e47c479eb030783f207f2587e24e5f112
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2021-12-16 21:09:52 +0000

    package

--- debian_setup_dev.sh
+++ debian_setup_dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl -s https://raw.githubusercontent.com/ng5/os/main/debian_setup.sh | bash
+curl -s https://raw.githubusercontent.com/ng5/os/main/debian_setup_minimal.sh | bash
 apt install -y autoconf automake build-essential clang clang-format cryptsetup libblas-dev libcurl4-openssl-dev libgmp-dev liblapack-dev libmpfr-dev libssl-dev libtool llvm openjdk-11-jdk zlib1g-dev
